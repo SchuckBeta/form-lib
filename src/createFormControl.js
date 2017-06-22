08a6544abcef20d50509cc6f9b7ed9a2b2b04a06
simonguo <simonguo.2009@gmail.com>
2017-06-22 14:26:29 +0800

    Fix: Change event fires too many times when inputing Chinese characters. https://github.com/facebook/react/issues/3926

--- src/createFormControl.js
+++ src/createFormControl.js
@@ -2,16 +2,64 @@ import React from 'react';
 
 function createFormControl(Component) {
 
+  const isChrome = !!window.chrome && !!window.chrome.webstore;
+  let isOnComposition = false;
+
+
   return class extends React.Component {
-    handleChange(e) {
-      const { onChange } = this.props;
-      onChange && onChange(e.target.value);
+
+
+    constructor(props, context) {
+      super(props, context);
+      this.handleChange = this.handleChange.bind(this);
+      this.handleComposition = this.handleComposition.bind(this);
     }
+
+    handleComposition(e) {
+      const { onChange } = this.props;
+      if (e.type === 'compositionend') {
+        // composition is end
+        isOnComposition = false;
+        /**
+         * fixed for Chrome v53+ and detect all Chrome
+         * https://chromium.googlesource.com/chromium/src/afce9d93e76f2ff81baaa088a4ea25f67d1a76b3%5E%21/
+         */
+
+        if (e.target instanceof HTMLInputElement && !isOnComposition && isChrome) {
+          // fire onChange
+          onChange && onChange(e.target.value);
+        }
+      } else {
+        // in composition
+        isOnComposition = true;
+      }
+    }
+
+    handleChange(e) {
+      const { onChange = () => { } } = this.props;
+      const isInput = (e.target instanceof HTMLInputElement || e.target instanceof HTMLTextAreaElement);
+      const value = e.target.value;
+
+      if (isInput && !isOnComposition) {
+        onChange(value);
+      } else if (!isInput) {
+        onChange(value);
+      }
+
+      isOnComposition = false;
+
+    }
+
     render() {
+      const { className, ...props } = this.props;
       return (
         <Component
-          {...this.props}
-          onChange={this.handleChange.bind(this)}
+          {...props}
+          className={`form-control ${className}`}
+          onCompositionStart={this.handleComposition}
+          onCompositionUpdate={this.handleComposition}
+          onCompositionEnd={this.handleComposition}
+          onChange={this.handleChange}
         />
       );
     }
